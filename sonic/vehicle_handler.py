bb1b579a67f233a34e221344902e8fa55ae68737
jack ogina <jackogina60@gmail.com>
2018-12-01 08:30:58 +0300

    base vehicle handler

--- sonic/vehicle_handler.py
+++ sonic/vehicle_handler.py
@@ -1,4 +1,10 @@
 import abc
+import sys
+from sonic.log_handler import LogHandler
+
+log_handler = LogHandler()
+log_handler.create_logger(__name__)
+
 class AbstractVehicleHandler(abc.ABC):
 
 
@@ -23,18 +29,32 @@ class AbstractVehicleHandler(abc.ABC):
 class VehicleHandler(AbstractVehicleHandler):
 
     def __init__(self, *args, **kwargs):
-        pass
+        try:
+            self.on=True
+            self.parts=[]
+        except Exception as e:
+            log_handler.logError(sys.exc_info(), e)
 
     def add(self,*args, **kwargs):
-        pass
-
+        try:
+            pass
+        except Exception as e:
+            log_handler.logError(sys.exc_info(), e)
 
     def start(self,*args, **kwargs):
-        pass
-
+        try:
+            pass
+        except Exception as e:
+            log_handler.logError(sys.exc_info(), e)
 
     def update_parts(self,*args, **kwargs):
-        pass
+        try:
+            pass
+        except Exception as e:
+            log_handler.logError(sys.exc_info(), e)
 
     def shutdown(self,*args, **kwargs):
-        pass
+        try:
+            pass
+        except Exception as e:
+            log_handler.logError(sys.exc_info(), e)
