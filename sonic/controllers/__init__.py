83509c1fcb6e57d303c03225d6473ac7ee8164ad
Jack Ogina <jackogina60@gmail.com>
2018-12-01 15:02:20 +0000

    ps3 controller config bug fix

--- sonic/controllers/__init__.py
+++ sonic/controllers/__init__.py
@@ -1,1 +1,1 @@
-import PS3Joystick
+
--- sonic/manage.py
+++ sonic/manage.py
@@ -3,7 +3,7 @@ from car_parts.donkey_tub_library import TubGroup, TubWriter
 from car_parts.transform import Lambda,PIDController
 from car_parts.camera_handler.picamera_handler import CarPiCamera
 from car_parts.web_handler.donkeycar_web_handler import LocalWebController
-from controllers.ps3_controller import PS3Joystick
+from controllers.ps3_controller.controller import PS3JoystickController
 from vehicle_handler import VehicleHandler
 import os
 import config as cfg
@@ -14,12 +14,13 @@ def drive(cfg):
     clock=Timestamp()
     cam=CarPiCamera(res=cfg.CAMERA_RESOLUTION)
     web_ctr=LocalWebController(use_chaos=False)
+
     ps3_ctr = PS3JoystickController(
-        max_throttle=cfg.JOYSTICK_MAX_THROTTLE,
-        steering_scale=cfg.JOYSTICK_STEERING_SCALE,
-        throttle_axis=cfg.JOYSTICK_THROTTLE_AXIS,
-        auto_record_on_throttle=cfg.AUTO_RECORD_ON_THROTTLE
-		)
+	throttle_scale=cfg.JOYSTICK_MAX_THROTTLE,
+	steering_scale=cfg.JOYSTICK_STEERING_SCALE,
+	#throttle_axis=cfg.JOYSTICK_THROTTLE_AXIS,
+	auto_record_on_throttle=cfg.AUTO_RECORD_ON_THROTTLE
+	)
 
     v.add(ps3_ctr,
       inputs=['cam/image_array'],
