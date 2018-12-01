49560478fd90b60889be6d613890a4c475be341a
jack ogina <jackogina60@gmail.com>
2018-12-01 17:10:13 +0300

    ps3 controller

---
+++ sonic/controllers/__init__.py
@@ -0,0 +1,1 @@
+import PS3Joystick
--- sonic/manage.py
+++ sonic/manage.py
@@ -3,29 +3,41 @@ from car_parts.donkey_tub_library import TubGroup, TubWriter
 from car_parts.transform import Lambda,PIDController
 from car_parts.camera_handler.picamera_handler import CarPiCamera
 from car_parts.web_handler.donkeycar_web_handler import LocalWebController
+from controllers.ps3_controller import PS3Joystick
 from vehicle_handler import VehicleHandler
 import os
-from config import *
+import config as cfg
 
 
-def drive():
+def drive(cfg):
     v=VehicleHandler()
     clock=Timestamp()
-    cam=CarPiCamera(res=CAMERA_RESOLUTION)
-    ctr=LocalWebController(use_chaos=False)
+    cam=CarPiCamera(res=cfg.CAMERA_RESOLUTION)
+    web_ctr=LocalWebController(use_chaos=False)
+    ps3_ctr = PS3JoystickController(
+        max_throttle=cfg.JOYSTICK_MAX_THROTTLE,
+        steering_scale=cfg.JOYSTICK_STEERING_SCALE,
+        throttle_axis=cfg.JOYSTICK_THROTTLE_AXIS,
+        auto_record_on_throttle=cfg.AUTO_RECORD_ON_THROTTLE
+		)
 
-    v.add(ctr,
+    v.add(ps3_ctr,
+      inputs=['cam/image_array'],
+      outputs=['user/angle', 'user/throttle', 'user/mode', 'recording'],
+      threaded=True)
+
+    v.add(web_ctr,
             inputs=["cam/image_array"],
             outputs=["user/angle","user/throttle","user/mode","recording"],
             threaded=True)
     v.add(cam, outputs=['cam/image_array'], threaded=True)
     inputs = ['cam/image_array', 'user/angle', 'user/throttle', 'user/mode', 'timestamp']
     types = ['image_array', 'float', 'float',  'str', 'str']
-    tub= TubWriter(path=TUB_PATH,inputs=inputs,types=types)
+    tub= TubWriter(path=cfg.TUB_PATH,inputs=inputs,types=types)
     v.add(tub,inputs=inputs,run_condition="recording")
     v.start(
-            rate_hz=DRIVE_LOOP_HZ,
-            max_loop_count=MAX_LOOPS
+            rate_hz=cfg.DRIVE_LOOP_HZ,
+            max_loop_count=cfg.MAX_LOOPS
             )
-drive()
+drive(cfg)
 
