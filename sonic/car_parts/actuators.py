6558be59f1a12d4e24d43932e966c446aa284779
jack ogina <jackogina60@gmail.com>
2018-12-04 08:23:03 +0300

    created linear mapping function

--- sonic/car_parts/actuators.py
+++ sonic/car_parts/actuators.py
@@ -1,6 +1,16 @@
 import time
 from car_parts.abstract_car_part_handler import AbstractCarPartHandler
+
+
 import RPi.GPIO as GPIO
+
+def map_range(x, x_mn, x_mx, y_mn, y_mx):
+    # Linear mapping between two ranges of values
+    x_r = x_mx - x_mn
+    y_r = y_mx - y_mn
+    xy_ratio = x_r / y_r
+    y = ((x - x_mn) / xy_ratio + y_mn) / 1
+    return float(y)
 
 class PWM_MG996R_Steering:
     """
@@ -19,13 +29,17 @@ class PWM_MG996R_Steering:
         self.on=False
         
     def set_pulse(self,a):
-        if not a or not self.LEFT_ANGLE<=a<=self.RIGHT_ANGLE:self.pin.ChangeDutyCycle(((self.MAX_DT+self.MIN_DT)/2))
-        elif a == 0:
-            self.pin.ChangeDutyCycle(float((self.MAX_DT+self.MIN_DT)/2))
-        elif a<0:
-            self.pin.ChangeDutyCycle(float((self.MAX_DT+self.MIN_DT)/2-((self.MAX_DT-self.MIN_DT)/2*-a)))
-        elif a>0:
-            self.pin.ChangeDutyCycle(float((self.MAX_DT+self.MIN_DT)/2+((self.MAX_DT-self.MIN_DT)/2*a)))
+        map_range(a, self.LEFT_ANGLE,self.RIGHT_ANGLE, self.MIN_DT, self.MAX_DT)
+        if not a or not self.LEFT_ANGLE<=a<=self.RIGHT_ANGLE:
+            x=map_range(0,self.LEFT_ANGLE,self.RIGHT_ANGLE,self.MIN_DT,self.MAX_DT)
+            self.pin.ChangeDutyCycle(x)
+        elif a == 0 or a<0 or a>0:
+            x=map_range(a,self.LEFT_ANGLE,self.RIGHT_ANGLE,self.MIN_DT,self.MAX_DT)
+            self.pin.ChangeDutyCycle(x)
+        # elif a<0:
+        #     self.pin.ChangeDutyCycle(float((self.MAX_DT+self.MIN_DT)/2-((self.MAX_DT-self.MIN_DT)/2*-a)))
+        # elif a>0:
+        #     self.pin.ChangeDutyCycle(float((self.MAX_DT+self.MIN_DT)/2+((self.MAX_DT-self.MIN_DT)/2*a)))
     def run(self,a):
         self.set_pulse(a)
     def start(self,*args, **kwargs):
@@ -48,7 +62,7 @@ class PWM_L298N_Throttle:
     F_THROTTLE = 1
     B_THROTTLE = -1
 
-    def __init__(self,pwm_pin=25,f_pin=23,b_pin=24,freq=1000,MIN_DT=10,MAX_DT=90):
+    def __init__(self,pwm_pin=25,f_pin=23,b_pin=24,freq=2000,MIN_DT=0,MAX_DT=100):
         self.pwm_pin=pwm_pin
         self.f_pin=f_pin
         self.b_pin=b_pin
@@ -68,17 +82,18 @@ class PWM_L298N_Throttle:
         self.on=False
 
     def set_pulse(self,t):
-        if not t or not self.B_THROTTLE<=t<=self.F_THROTTLE:self.pin.ChangeDutyCycle(0)
-        elif t == 0:
+        if not t or not self.B_THROTTLE<=t<=self.F_THROTTLE or t==0:
             self.pin.ChangeDutyCycle(0)
         elif t<0:
             GPIO.output(self.f_pin,GPIO.LOW)
             GPIO.output(self.b_pin,GPIO.HIGH)
-            self.pin.ChangeDutyCycle(float((self.MAX_DT+self.MIN_DT)/2+((self.MAX_DT-self.MIN_DT)/2*-t)))
+            x=map_range(-t,0,1,self.MIN_DT,self.MAX_DT)
+            self.pin.ChangeDutyCycle(x)
         elif t>0:
             GPIO.output(self.f_pin,GPIO.HIGH)
             GPIO.output(self.b_pin,GPIO.LOW)
-            self.pin.ChangeDutyCycle(float((self.MAX_DT+self.MIN_DT)/2+((self.MAX_DT-self.MIN_DT)/2*t)))
+            x=map_range(t,0,1,self.MIN_DT,self.MAX_DT)
+            self.pin.ChangeDutyCycle(x)
     def run(self,t):
         self.set_pulse(t)
     def start(self,*args, **kwargs):
